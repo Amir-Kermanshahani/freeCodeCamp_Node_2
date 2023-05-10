469dcb3ebf5aed2e615a33fa8370d591ab3982b9
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-10 19:12:45 +0430

    === changed to ==

--- myApp.js
+++ myApp.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', function(req, res) {
-    if (process.env.MESSAGE_STYLE === uppercase) {
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
         response = "Hello json".toUpperCase();
       } else {
         response = "Hello json";
