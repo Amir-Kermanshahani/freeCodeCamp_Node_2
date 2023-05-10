73131b4fc14e908c97f2d1bc4ead86f7efb70fcd
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-10 18:08:46 +0430

    response edited

--- myApp.js
+++ myApp.js
@@ -12,9 +12,9 @@ app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', function(req, res) {
     if (process.env.MESSAGE_STYLE === "uppercase") {
-        response = "Hello World".toUpperCase();
+        response = "Hello json".toUpperCase();
     } else {
-        response = "Hello World";
+        response = "Hello json";
     }
     res.json({"message" : response});
 });
