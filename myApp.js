6e8d12d635bf08f940021945a961107d536437f3
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-10 20:05:40 +0430

    7

--- myApp.js
+++ myApp.js
@@ -1,17 +1,16 @@
-const { response } = require('express');
 let express = require('express');
 let app = express();
 require('dotenv').config();
 
 
 
-console.log("Hello World");
+// console.log("Hello World");
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-});
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/views/index.html');
+// });
 
-app.use('/public', express.static(__dirname + '/public'));
+// app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', function(req, res) {
     if (process.env.MESSAGE_STYLE === "uppercase") {
