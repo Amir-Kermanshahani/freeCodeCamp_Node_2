e5cf91eca0fb9f2ebcaf2eb85736704b7f9bbd6a
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-10 20:14:59 +0430

    8

--- myApp.js
+++ myApp.js
@@ -4,23 +4,28 @@ require('dotenv').config();
 
 
 
-// console.log("Hello World");
+console.log("Hello World");
 
-// app.get('/', function(req, res) {
-//     res.sendFile(__dirname + '/views/index.html');
-// });
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
-// app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
+/** 5) serve JSON on a specific route */
+/*
+app.get('/json', function (req, res) {
+  res.json({"message": "Hello json"});
+})
+*/
+
+/** 6) Use the .env file to configure the app */
 app.get('/json', function(req, res) {
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        var response = "Hello json".toUpperCase();
-    }
-    else {
-        var response = "Hello json";
-    }
-    res.json({"message" : response});
-;})
+    if (process.env.MESSAGE_STYLE === "uppercase")
+      res.json({"message": "HELLO JSON"})
+    else
+      res.json({"message": "Hello json"})
+  })
 
 
 
