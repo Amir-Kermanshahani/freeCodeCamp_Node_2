c11470c1ed9dd327231da4dcc2b0b606df1ebff9
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-10 20:18:08 +0430

    9

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 let express = require('express');
 let app = express();
 require('dotenv').config();
@@ -21,11 +22,14 @@ app.get('/json', function (req, res) {
 
 /** 6) Use the .env file to configure the app */
 app.get('/json', function(req, res) {
-    if (process.env.MESSAGE_STYLE === "uppercase")
-      res.json({"message": "HELLO JSON"})
-    else
-      res.json({"message": "Hello json"})
-  })
+    var jsonResponse = {"message" : "Hello json"};
+
+    if (process.env.MESSAGE_STYLE === "uupercase") {
+        jsonResponse.message = jsonResponse.message.toUpperCase()
+    }
+
+    res.json(jsonResponse);
+});
 
 
 
