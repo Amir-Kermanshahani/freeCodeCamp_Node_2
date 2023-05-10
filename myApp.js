6c65150ed605603ea0874a0535436af446890b74
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-10 20:20:15 +0430

    10

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
-const { json } = require('body-parser');
 let express = require('express');
 let app = express();
+let bGround = require('fcc-express-bground');
 require('dotenv').config();
 
 
@@ -13,14 +13,13 @@ app.get('/', function(req, res) {
 
 app.use('/public', express.static(__dirname + '/public'));
 
-/** 5) serve JSON on a specific route */
 /*
 app.get('/json', function (req, res) {
   res.json({"message": "Hello json"});
 })
 */
 
-/** 6) Use the .env file to configure the app */
+
 app.get('/json', function(req, res) {
     var jsonResponse = {"message" : "Hello json"};
 
