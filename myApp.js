1d17695c93ced5976e3624e72854c23d3d32224f
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-10 20:51:30 +0430

    edited

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ var app = express();
 
 console.log('Hello World');
 
-app.get("/",(req,res)=>res.sendFile(`${__dirname}/views/index.html`));
+app.get("/",(req,res)=>res.sendFile(__dirname + '/views/index.html'));
 
-app.use(express.static(__dirname+"/public"));
+app.use("/public", express.static( __dirname + "/public"));
 
  app.get("/json",(req,res)=>{
    let resultMessage="Hello json";
