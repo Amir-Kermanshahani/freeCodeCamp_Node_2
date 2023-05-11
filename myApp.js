fca1b325a7c596a5a28c182c6090ac16a21b1346
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-11 12:54:44 +0430

    1

--- myApp.js
+++ myApp.js
@@ -1,31 +1,29 @@
-
-require('fcc-express-bground');
-require('dotenv')
 var express = require('express');
 var app = express();
 
+
 console.log('Hello World');
 
-app.get("/",(req,res)=>res.sendFile(__dirname + '/views/index.html'));
 
-app.use("/public", express.static( __dirname + "/public"));
+app.get("/",(req,res)=>res.sendFile(`${__dirname}/views/index.html`));
+
+app.use(express.static(__dirname+"/public"));
+
+ app.get("/json",(req,res)=>{
+   let resultMessage="Hello json";
+   if (process.env.MESSAGE_STYLE==="uppercase"){
+     resultMessage=resultMessage.toUpperCase();
+   }
+  
+   res.json({"message": resultMessage});
+                            
+ });
+
+ // This would be part of the basic setup of an Express app
+// but to allow FCC to run tests, the server is already active
+/** app.listen(process.env.PORT || 3000 ); */
+
+//---------- DO NOT EDIT BELOW THIS LINE --------------------
 
 
-app.get('/json', function (req, res) {
-
-    let response = {"message": "Hello json"};
-
-    if(process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({"message": "Hello json".toUpperCase()});
-    }
-    else {
-        res.json({"message": "Hello json"});
-    }
-
-    res.json(response);
-    
-})
-
-
-
-module.exports = app;
+ module.exports = app;
