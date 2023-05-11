4e234c62fc2796f41a25740c50dd83a885b636aa
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-11 14:09:48 +0430

    6

--- myApp.js
+++ myApp.js
@@ -15,11 +15,12 @@ app.get("/",(req,res)=>res.sendFile(__dirname + '/views/index.html'));
 
 
 app.get("/json", (req,res) => {
-    if (process.env['MESSAGE_STYLE'] == "uppercase") {
-        res.json({"message" : "Hello json".toUpperCase()});
+    const mySecret = process.env['MESSAGE_STYLE']
+    if (mySecret == "uppercase") {
+        res.json({"message": "HELLO JSON"});
     }
     else {
-        res.json({"message" : "Hello json"});
+        res.json({"message": "Hello json"});
     }
     
 });
