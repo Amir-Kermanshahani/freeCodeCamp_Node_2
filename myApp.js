02255f63ae46168e92388d71df11e1b48d8038a4
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-05-11 13:10:25 +0430

    3

--- myApp.js
+++ myApp.js
@@ -11,18 +11,13 @@ app.get("/",(req,res)=>res.sendFile(`${__dirname}/views/index.html`));
 
 app.use("/public", express.static(__dirname+"/public"));
 
- app.get("/json",(req,res)=>{
-   var resultMessage=null;
-   if (process.env.MESSAGE_STYLE==="uppercase"){
-        resultMessage="Hello json".toUpperCase();
-   }
-   else {
-        resultMessage="Hello json";
-   }
-  
-   res.json({message: resultMessage});
-                            
- });
+app.get("/json",(req,res)=>{
+    let response = {"message" : "Hello json"};
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        response.message = response.message.toUpperCase();
+    } 
+    res.json(message);
+});
 
  // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
